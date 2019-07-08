22b7fbd5b707ae2143c627535db39ffd7cf5fda1
Anand Chaudhary <anand92490@gmail.com>
2019-07-08 17:31:37 -0500

    At this stage I have created the 4 boxes that have a number assigned to it.

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+// create a game with 4 cristal images that generates random number.
+// Display a random number between 19 and 120.
+// Each cristal should have a random hidden value assigned to it between 1-12.
+// The game should reset and generate a new random number when the game is completed.
+// The value that each cristal holds should be displayed when clicked and added to the value of the next cristal of the users choice.
+// If the value of the cristal is === that of the random number the than increment the value of the win counter
+// If the value is greater than the random number decrement the lost counter. 
+
+var result;
+var win;
+var loss;
+
+for(var i = 0; i < 4; i++){
+
+    var someNum = Math.floor(Math.random() * 120);
+    // console.log(someNum);
+   
+    var crystalCounter  = $("<div>");
+  
+        crystalCounter.attr({"class": "crystalCounter", "numInBox": someNum });
+
+ 
+   $(".crystalBox").append(crystalCounter);
+ 
+}
