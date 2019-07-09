0601bb9ea65f9613f9d107e56f886c143953905a
Anand Chaudhary <anand92490@gmail.com>
2019-07-08 19:47:23 -0500

    got the basic functions of the game to work at this point.The value still needs to append to it's original div.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,20 @@
 // create a game with 4 cristal images that generates random number.
 // Display a random number between 19 and 120.
 // Each cristal should have a random hidden value assigned to it between 1-12.
-// The game should reset and generate a new random number when the game is completed.
 // The value that each cristal holds should be displayed when clicked and added to the value of the next cristal of the users choice.
 // If the value of the cristal is === that of the random number the than increment the value of the win counter
 // If the value is greater than the random number decrement the lost counter. 
+// The game should reset and generate a new random number when the game is completed.
 
 var result;
 var win;
 var loss;
+var exNum = 0;
 
 guessThisNumber = Math.floor(Math.random() * 101) + 19;
-//  
 
 $("#randomNum").html("Guess this number: " + guessThisNumber); 
+
 for(var i = 0; i < 4; i++){
 
     var someNum = Math.floor(Math.random() * 11) + 1;
@@ -27,3 +28,18 @@ for(var i = 0; i < 4; i++){
    $(".crystalBox").append(crystalCounter);
  
 }
+
+$(".crystalCounter").on("click", function(){
+
+    var boxGenNum = parseInt($(this).attr("numInBox"));
+
+    exNum = exNum + boxGenNum;
+     console.log(exNum);
+
+     if(exNum > guessThisNumber){
+         console.log("you loose");
+    }
+    else if(exNum === guessThisNumber){
+        console.log("you won");
+    }
+});
